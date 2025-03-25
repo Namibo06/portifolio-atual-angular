3981f34156bb95665423aeea36dd2c6547a128ed
Namibo06 <namibo654@gmail.com>
2025-03-25 15:13:45 -0300

    refactor: created routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home/home.component';
+import { BlogComponent } from './pages/blog/blog/blog.component';
+import { ArticleComponent } from './pages/article/article/article.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
