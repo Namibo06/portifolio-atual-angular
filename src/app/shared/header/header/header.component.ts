e88752548d7de923f441e02d33a4129c6f88a807
Namibo06 <namibo654@gmail.com>
2025-03-25 15:30:17 -0300

    refactor: removed directive, and added logic by routerLinkActive

--- src/app/shared/header/header/header.component.ts
+++ src/app/shared/header/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit{
   linkActive: string = "";
 
+  constructor(private router: Router) {} 
+
   ngOnInit(): void {
     this.activedLink('home');
   }
 
   activedLink(nameLink: string){
     this.linkActive = nameLink;
+
   }
 }
