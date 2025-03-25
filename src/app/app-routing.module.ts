005a22f75b320fb9e05096d8af31f13aae92ecc5
Namibo06 <namibo654@gmail.com>
2025-03-25 15:17:10 -0300

    refactor: removed unnecessary imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home/home.component';
-import { BlogComponent } from './pages/blog/blog/blog.component';
-import { ArticleComponent } from './pages/article/article/article.component';
 
 const routes: Routes = [
   {
