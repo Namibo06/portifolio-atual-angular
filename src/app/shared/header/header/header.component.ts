e556af48f27ac90c8d9b5a129b576e7cf15e10b4
Namibo06 <namibo654@gmail.com>
2025-03-25 15:08:27 -0300

    feat: created header component

---
+++ src/app/shared/header/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent implements OnInit{
+  linkActive: string = "";
+
+  ngOnInit(): void {
+    this.activedLink('home');
+  }
+
+  activedLink(nameLink: string){
+    this.linkActive = nameLink;
+  }
+}
