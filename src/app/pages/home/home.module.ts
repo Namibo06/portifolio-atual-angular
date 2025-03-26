69ff30ec3d2499960038067ab9ef9920096725c2
Namibo06 <namibo654@gmail.com>
2025-03-26 15:29:32 -0300

    feat: added modules in home module

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderModule } from '../../shared/header/header.module';
 import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CardTemplateModule } from '../../components/card-template/card-template.module';
 
 
 
@@ -10,7 +13,10 @@ import { HomeComponent } from './home/home.component';
   exports: [HomeComponent],
   imports: [
     CommonModule,
-    HeaderModule
+    HeaderModule,
+    FormsModule,
+    RouterModule,
+    CardTemplateModule
   ]
 })
 export class HomeModule { }
