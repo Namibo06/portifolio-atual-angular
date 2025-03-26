c199feb4c79ebfa9c6d830975a4ef1dff0a3c5c4
Namibo06 <namibo654@gmail.com>
2025-03-26 01:36:58 -0300

    refactor: route ' ' redirected  for route 'home'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home/home.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
+  { 
+    path: '', 
+    redirectTo: 'home', 
+    pathMatch: 'full' 
   },
   {
     path: 'home',
