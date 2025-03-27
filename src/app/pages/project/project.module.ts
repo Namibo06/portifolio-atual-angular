853375a5f88b2110d9862d18a1f693a8f6018a7b
Namibo06 <namibo654@gmail.com>
2025-03-27 15:14:19 -0300

    feat: added content in project component

--- src/app/pages/project/project.module.ts
+++ src/app/pages/project/project.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ProjectComponent } from './project/project.component';
+import { HeaderModule } from '../../shared/header/header.module';
+import { AboutMeModule } from '../../components/about-me/about-me.module';
+import { CardTemplateModule } from '../../components/card-template/card-template.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProjectComponent],
+  exports: [ProjectComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    HeaderModule,
+    AboutMeModule,
+    CardTemplateModule
   ]
 })
 export class ProjectModule { }
--- src/app/pages/project/project/project.component.ts
+++ src/app/pages/project/project/project.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrl: './project.component.scss'
 })
 export class ProjectComponent {
+  /*search*/
+  searchContent: string = "";
+  searchFoundLength: number = 0;
+  contentInput: string = "";
+
+  findBySearch(){
+    this.searchContent = this.contentInput;
+  }
+
+  projects = [
+    {
+      id: "1",
+      image_url: "barbx.jpg",
+      types: [
+        { id: "1", type_name: "HTML" },
+        { id: "2", type_name: "CSS" },
+        { id: "3", type_name: "JavaScript" }
+      ],
+      title: "Barbx",
+      description: "Recentemente, percebi que o pessoal de T.I tem uma breve dificuldade em começar a programar, então sempre recomendo a todos, a primeiro entender como funciona a internet e também como funciona HTML, CSS e JavaScript...",
+      nameToPost: "Ver projeto"
+    },
+    {
+      id: "2",
+      image_url: "barbx.jpg",
+      types: [
+        { id: "1", type_name: "Node.js" },
+        { id: "2", type_name: "Express.js" },
+        { id: "3", type_name: "Sequelize" },
+        { id: "4", type_name: "MySQL" }
+      ],
+      title: "Gerenciador para um hotel canino",
+      description: "Recentemente, percebi que o pessoal de T.I tem uma breve dificuldade em começar a programar, então sempre recomendo a todos, a primeiro entender como funciona a internet e também como funciona HTML, CSS e JavaScript...",
+      nameToPost: "Ver projeto"
+    },
+    {
+      id: "3",
+      image_url: "barbx.jpg",
+      types: [
+        { id: "1", type_name: "Java" },
+        { id: "2", type_name: "Kafka" },
+        { id: "3", type_name: "SpringBoot" },
+        { id: "4", type_name: "MySQL" },
+      ],
+      title: "E-commerce com pagamento",
+      description: "Recentemente, percebi que o pessoal de T.I tem uma breve dificuldade em começar a programar, então sempre recomendo a todos, a primeiro entender como funciona a internet e também como funciona HTML, CSS e JavaScript...",
+      nameToPost: "Ver projeto"
+    },
+  ];
 
 }
