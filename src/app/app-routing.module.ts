ba04e2755ed5e2d80c668991eb969085e31acaa2
Namibo06 <namibo654@gmail.com>
2025-03-27 13:41:52 -0300

    feat: added route for projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home/home.component';
+import { ProjectComponent } from './pages/project/project/project.component';
 
 const routes: Routes = [
   { 
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'projetos',
+    component: ProjectComponent
   },
   {
     path: '**',
