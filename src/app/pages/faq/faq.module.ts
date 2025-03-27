7badae08c6a8d47ee6517e26c5a5abe7693ed053
Namibo06 <namibo654@gmail.com>
2025-03-27 18:29:58 -0300

    feat: added content in component faq

--- src/app/pages/faq/faq.module.ts
+++ src/app/pages/faq/faq.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderModule } from '../../shared/header/header.module';
+import { RouterModule } from '@angular/router';
+import { FaqComponent } from './faq/faq.component';
+import { RelevantArticlesModule } from '../../components/relevant-articles/relevant-articles.module';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FaqComponent],
+  exports: [FaqComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HeaderModule,
+    RouterModule,
+    RelevantArticlesModule,
+    FormsModule
   ]
 })
 export class FaqModule { }
--- src/app/pages/faq/faq/faq.component.ts
+++ src/app/pages/faq/faq/faq.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrl: './faq.component.scss'
 })
 export class FaqComponent {
+  /*search*/
+  searchContent: string = "";
+  searchFoundLength: number = 0;
+  contentInput: string = "";
 
+  /*faq*/
+  openFaqs: { [key: string]: boolean } = {}; 
+
+  findBySearch(){
+    this.searchContent = this.contentInput;
+  }
+
+  showHideFaqContent(id: string) {
+    this.openFaqs = { [id]: !this.openFaqs[id] };
+  }
+
+  faqData = [
+    {
+      id: "1",
+      title: 'Estou disponível para trabalhar presencialmente, híbrido ou remoto?',
+      description: 'Se a vaga for em Feira de Santana estou disposto a trabalhar de forma presencial ou híbrida, fora isso, estou disposto a viajar de tempos em tempos para visitar o local de trabalho mas mantendo-se em formato remoto'
+    },
+    {
+      id: "2",
+      title: 'Aceito PJ? Estou atrás de estágio? Para quais contratos de trabalho estou disponível?',
+      description: 'Estou aberto aos seguintes tipos de contrato de trabalho: CLT, PJ, estágio, jovem aprendiz, auxiliar, assistente'
+    },
+    {
+      id: "3",
+      title: 'Quais áreas já tive experiência acadêmica, profissional ou pessoal',
+      description: " Frontend: HTML - CSS - JavaScript - Sass - Bootstrap - JQuery - Angular Framework - VueJS. Backend:  PHP - Java - Node.js - Nest.js - Laravel - SpringBoot. Banco de Dados: SQLite - MongoDB - MySQL - PostgreSQL. Ferramentas: Git - Docker - RabbitMQ - Slack - Trello - Gitlab - Github - Android Studio - dentre outros"
+    },
+    {
+      id: "4",
+      title: 'Faixa salarial: Tem mínima definida? Tem alguns valores em mente?',
+      description: 'Para estágio e jovem aprendiz no mínimo deve ser R$900, para regime CLT mínimo de R$2.500, e PJ, mínimo de R$2.900'
+    },
+  ];
 }
