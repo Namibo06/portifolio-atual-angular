682d0afd250a1cdf172e52c7c2bff4786dd84f49
Namibo06 <namibo654@gmail.com>
2025-04-02 14:34:49 -0300

    feat: created component for create article

--- src/app/pages/create-article/create-article/create-article.component.ts
+++ src/app/pages/create-article/create-article/create-article.component.ts
@@ -6,5 +6,76 @@ import { Component } from '@angular/core';
   styleUrl: './create-article.component.scss'
 })
 export class CreateArticleComponent {
+  //modal
+  isOpenModal: boolean = false;
 
+  //templates
+  isOpenTemplateTitle: boolean = false;
+  isOpenTemplateSubtitle: boolean = false;
+  isOpenTemplateParagraph: boolean = false;
+  isOpenTemplateImage: boolean = false;
+
+  //data
+  titleForm: string = "";
+  subtitleForm: string = "";
+  paragraphForm: string = "";
+
+  constructor(){}
+
+  hideAll(){
+    this.isOpenTemplateTitle = false;
+    this.isOpenTemplateSubtitle = false;
+    this.isOpenTemplateParagraph = false;
+    this.isOpenTemplateImage = false;
+  }
+
+  openTemplateTitle(){
+    this.isOpenTemplateTitle = true;
+    this.isOpenTemplateSubtitle = false;
+    this.isOpenTemplateParagraph = false;
+    this.isOpenTemplateImage = false;
+  }
+
+  openTemplateSubtitle(){
+    this.isOpenTemplateTitle = false;
+    this.isOpenTemplateSubtitle = true;
+    this.isOpenTemplateParagraph = false;
+    this.isOpenTemplateImage = false;
+  }
+
+  openTemplateParagraph(){
+    this.isOpenTemplateTitle = false;
+    this.isOpenTemplateSubtitle = false;
+    this.isOpenTemplateParagraph = true;
+    this.isOpenTemplateImage = false;
+  }
+
+  openTemplateImage(){
+    this.isOpenTemplateTitle = false;
+    this.isOpenTemplateSubtitle = false;
+    this.isOpenTemplateParagraph = false;
+    this.isOpenTemplateImage = true;
+  }
+
+  chooseTemplate(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+  
+    this.hideAll(); 
+  
+    switch (selectedValue) {
+      case "1":
+        this.openTemplateTitle();
+        break;
+      case "2":
+        this.openTemplateSubtitle();
+        break;
+      case "3":
+        this.openTemplateParagraph();
+        break;
+      case "4":
+        this.openTemplateImage();
+        break;
+    }
+  }
+  
 }
