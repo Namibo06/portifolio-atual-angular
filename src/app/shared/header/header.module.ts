0fab9514a9ef218f6df7ede2409544a7d05909c0
Namibo06 <namibo654@gmail.com>
2025-04-17 23:27:13 -0300

    feat: redesigned header

--- src/app/shared/header/header.module.ts
+++ src/app/shared/header/header.module.ts
@@ -2,6 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
@@ -10,7 +11,8 @@ import { RouterModule } from '@angular/router';
   exports: [HeaderComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FontAwesomeModule
   ]
 })
 export class HeaderModule { }
--- src/app/shared/header/header/header.component.ts
+++ src/app/shared/header/header/header.component.ts
@@ -1,12 +1,14 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent implements AfterViewInit{
-  linkActive: string = ""; 
+export class HeaderComponent implements OnInit{
+  /*linkActive: string = ""; 
 
   ngAfterViewInit(): void {
     this.activedLink('home');
@@ -14,5 +16,35 @@ export class HeaderComponent implements AfterViewInit{
 
   activedLink(nameLink: string){
     this.linkActive = nameLink;
+  }*/
+
+  moonIcon:IconDefinition = faMoon;
+  sunIcon:IconDefinition = faSun;
+  savedTheme: string | null = localStorage.getItem('header__theme__icon__portifolio__blog__joca') || "moon";
+  iconActual: string = "";
+  templateTheme: IconDefinition = (localStorage.getItem('header__theme__icon__portifolio__blog__joca') === "sun") ? this.sunIcon : (localStorage.getItem('header__theme__icon__portifolio__blog__joca') === "moon") ? this.moonIcon : (localStorage.getItem('header__theme__icon__portifolio__blog__joca') === "null") ? this.moonIcon : this.moonIcon;
+
+  ngOnInit(){
+    this.iconActual = (localStorage.getItem('header__theme__icon__portifolio__blog__joca') === "sun") ? "sun" : (localStorage.getItem('header__theme__icon__portifolio__blog__joca') === "moon") ? "moon" : (localStorage.getItem('header__theme__icon__portifolio__blog__joca') === "null") ? "moon" : "moon";
+  }
+
+
+  changeTheme(){
+    if(this.iconActual === "sun"){
+      this.iconActual = "moon";
+      this.templateTheme = this.moonIcon;
+    }else if(this.iconActual === "moon"){
+      this.iconActual = "sun";
+      this.templateTheme = this.sunIcon;
+    }
+    this.saveTheme(this.iconActual);
+    console.log(this.iconActual);
+  }
+
+  saveTheme(theme: string){
+    if(localStorage.getItem('header__theme__icon__portifolio__blog__joca')){
+      localStorage.removeItem('header__theme__icon__portifolio__blog__joca');
+    }
+    localStorage.setItem('header__theme__icon__portifolio__blog__joca', theme);
   }
 }
